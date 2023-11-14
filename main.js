7464ada8872ef92bfe6eef05bbec6de97f05e5cf
Chriscraft <123362394+Chriscraft57@users.noreply.github.com>
2023-11-14 11:49:03 +0100

    init

---
+++ main.js
@@ -0,0 +1,31 @@
+const app = new PIXI.Application();
+document.body.appendChild(app.view);
+
+app.loader.add('example', 'example.jpg').load((loader, resources) => {
+    const example = new PIXI.Sprite(resources.example.texture);
+
+    app.stage.addChild(example);
+
+    Controller.Mouse.prevent(BUTTON.RIGHT);
+
+    app.ticker.add(() => {
+      if(Controller.Mouse.isButtonDown(BUTTON.LEFT)) {
+        console.log('left')
+      }
+
+      if(Controller.Mouse.isButtonDown(BUTTON.RIGHT)) {
+        console.log('right')
+      }
+      
+      if (Controller.Keyboard.isKeyDown(...PLAYER.LEFT))
+        example.x -= 1;
+      if (Controller.Keyboard.isKeyDown(...PLAYER.RIGHT))
+        example.x += 1;
+      if (Controller.Keyboard.isKeyDown(...PLAYER.UP))
+        example.y -= 1;
+      if (Controller.Keyboard.isKeyDown(...PLAYER.DOWN))
+        example.y += 1;
+
+      Controller.update();
+    });
+});
